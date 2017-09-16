52f74efb50c830871a718c3c48bc8ae3093f43e6
Suraj Paramasivam <sujisuraj@gmail.com>
2017-09-16 08:58:03 -0400

    draft

---
+++ TF_Experiments/dl7_rnn1.py
@@ -0,0 +1,12 @@
+import collections
+import math
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import zipfile
+from matplotlib import pylab
+from six.moves import range
+from six.moves.urllib import urlretrieve
+
+
---
+++ TF_Experiments/scrap.py
@@ -0,0 +1,4 @@
+import bson
+import multiprocessing
+from skimage.data import imread
+import io
